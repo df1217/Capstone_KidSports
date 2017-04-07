7a880c3a82a1edb4ae8a5cf72955d6fef42544b2
df1217 <dffrank1217@gmail.com>
2017-04-07 15:49:17 -0700

    add domain models

---
+++ Capstone/src/KidSports/Models/Application.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KidSports.Models
+{
+    public class Application
+    {
+        public int ApplicationID { get; set; }
+        public List<string> StatesLived { get; set; }
+        public List<string> CountriesLived { get; set; }
+        public List<string> PreviousLastNames { get; set; }
+
+
+        public string BackgroundRequest { get; set; }
+
+        public string BackgroundResult { get; set; }
+        public DateTime DOB { get; set; }
+
+        public string CurrentEmployer { get; set; }
+        public string JobTitle { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public School AppSchool { get; set; }
+        public Sport AppSport { get; set; }
+
+        public List<Area> Region { get; set; }
+
+    }
+}
