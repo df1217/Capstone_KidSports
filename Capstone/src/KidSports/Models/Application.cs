1498a0de8a582d2d523cf9eeae74cb1ba2e4ffef
df1217 <dffrank1217@gmail.com>
2017-04-08 16:20:47 -0700

    add properties to domain model

--- Capstone/src/KidSports/Models/Application.cs
+++ Capstone/src/KidSports/Models/Application.cs
@@ -31,7 +31,57 @@ namespace KidSports.Models
         public School AppSchool { get; set; }
         public Sport AppSport { get; set; }
 
+        public Nullable<int> PreviousYearsCoached { get; set; }
+        public Nullable<int> PreviousGradesCoached { get; set; }
+        public string NameOfChild { get; set; }
+
+        public int YearsExperience { get; set; }
+
+        public DateTime AppStartDate { get; set; }
+        public DateTime AppApprovalDate { get; set; }
+
+        public bool AppIsChecked { get; set; }
+        public DateTime BcSubmissionDate { get; set; }
+        public DateTime BcApprovalDate { get; set; }
+        public DateTime DlSubmissionDate { get; set; }
+        public DateTime DlApprovalDate { get; set; }
+        public string  DlPath { get; set; }
+
+        public DateTime PcaSubmissionDate { get; set; }
+        public DateTime PcaApprovalDate { get; set; }
+        public bool PcaIsChecked { get; set; }
+        public string PcaPath { get; set; }
+
+        public string PcaVoucherCode { get; set; }
+
+        public DateTime NfhsSubmissionDate { get; set; }
+        public DateTime NfhsApprovalDate { get; set; }
+
+        public bool NfhsIsChecked { get; set; }
+        public string NfhsPath { get; set; }
+        public DateTime BadgeSubmissionDate { get; set; }
+        public DateTime BadgeApprovalDate { get; set; }
+        public string BadgePath { get; set; }
+
+        public DateTime PrefSubmissionDate { get; set; }
+        public DateTime PrefApprovalDate { get; set; }
+
+        public DateTime PledgeSubmissionDate { get; set; }
+        public DateTime PledgeApprovalDate { get; set; }
+        public string PledgeInitials { get; set; }
+
         public List<Area> Region { get; set; }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
