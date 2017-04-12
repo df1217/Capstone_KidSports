674030a866dea8c121e2eec03c9f2435f1fd16f9
df1217 <dffrank1217@gmail.com>
2017-04-11 21:34:40 -0700

    add upload functionality, solution not building without errors

---
+++ Capstone/src/KidSports/ViewModels/Page6ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using KidSports.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace KidSports.ViewModels
+{
+    public class Page6ViewModel
+    {
+        public Application Page6 { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
