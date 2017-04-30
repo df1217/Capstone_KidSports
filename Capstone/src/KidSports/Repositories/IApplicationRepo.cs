42f374a9d169c3a52f688c1cb8ac3de84c7df8fb
Devin <devin.a.dubose@gmail.com>
2017-04-30 11:13:21 -0700

    Add filtering applications

---
+++ Capstone/src/KidSports/Repositories/IApplicationRepo.cs
@@ -0,0 +1,15 @@
+﻿using KidSports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KidSports.Repositories
+{
+    public interface IApplicationRepo
+    {
+        List<Application> GetAllApplications();
+        IQueryable<Application> GetFilteredApplications(ApplicantSearchModel searchModel);
+
+    }
+}
