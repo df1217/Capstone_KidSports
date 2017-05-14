c4a0250ad51ca27290527a8aaa82c00ef380382b
Emma3165 <anucharles@hotmail.com>
2017-05-13 23:02:13 -0700

    update coachinfo and coachinterest

---
+++ src/KidSports/Models/ViewModels/UpdateAppContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KidSports.Models.ViewModels
+{
+    public class UpdateAppContent
+    {
+        public Area AreaName { get; set; }
+        public School SchoolName { get; set; }
+        public Sport SportName { get; set; }
+        
+    }
+}
---
+++ src/KidSports/Models/ViewModels/UpdateApplink.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KidSports.Models.ViewModels
+{
+    public class UpdateApplink
+    {
+        public string Address { get; set; }
+        public string BevBlog { get; set; }
+        public string Email { get; set; }
+        public string FaceBookCoach { get; set; }
+        public string FaceBookKidsports { get; set; }
+        public string Instragram { get; set; }
+        public string NFHS { get; set; }
+        public string PCA { get; set; }
+        public int Phone { get; set; }
+        public string Twitter { get; set; }
+        public string  Voucher { get; set; }
+    }
+}
