6905c5360b6acf1480cea722c6a9d4a4c6e7132a
df1217 <dffrank1217@gmail.com>
2017-05-27 15:24:59 -0700

    Add validation

--- Capstone/src/KidSports/Models/ViewModels/RegisterViewModel.cs
+++ Capstone/src/KidSports/Models/ViewModels/RegisterViewModel.cs
@@ -1,11 +1,30 @@
-﻿namespace KidSports.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KidSports.Models.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [RegularExpression("^([A-Za-z]){,20}$", ErrorMessage = "Name must be test less than 20 alpha characters") ]
         public string FirstName { get; set; }
+        [Required]
         public string MiddleName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("\\A[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a - z0 - 9](?:[a - z0 - 9 -] *[a - z0 - 9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a - z0 - 9])?\\z",
+            ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$",
+        ErrorMessage = "Password must be at least 8 characters and contain an uppercase, lowercase, digit, and special character")]
+      //  [DataType(DataType.Password, ErrorMessage = "Blah")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
