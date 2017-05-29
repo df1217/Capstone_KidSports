a271ca069a01b2bea4251a792821d25a129991bb
df1217 <dffrank1217@gmail.com>
2017-05-28 18:32:37 -0700

    Add validation

--- Capstone/src/KidSports/Models/ViewModels/RegisterViewModel.cs
+++ Capstone/src/KidSports/Models/ViewModels/RegisterViewModel.cs
@@ -5,22 +5,27 @@ namespace KidSports.Models.ViewModels
     public class RegisterViewModel
     {
         [Required]
-        [RegularExpression("^([A-Za-z]){,20}$", ErrorMessage = "Name must be test less than 20 alpha characters") ]
+        [StringLength(20, ErrorMessage = "Name must be less than 20 characters")]
+        [RegularExpression("^([A-Z]{1}[a-zA-Z ]*$)", ErrorMessage = "Name must be capitalized and not contain any digits or special characters")]
+       
         public string FirstName { get; set; }
-        [Required]
+
+        [StringLength(20, ErrorMessage = "Name must be less than 20 characters")]
+        [RegularExpression("^([A-Z]{1}[a-zA-Z ]*$)", ErrorMessage = "Name must be capitalized and not contain any digits or special characters")]
         public string MiddleName { get; set; }
         [Required]
+        [StringLength(20, ErrorMessage = "Name must be less than 20 characters")]
+        [RegularExpression("^([A-Z]{1}[a-zA-Z ]*$)", ErrorMessage = "Name must be capitalized and not contain any digits or special characters")]
         public string LastName { get; set; }
         [Required]
         [DataType(DataType.EmailAddress)]
-        [RegularExpression("\\A[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a - z0 - 9](?:[a - z0 - 9 -] *[a - z0 - 9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a - z0 - 9])?\\z",
-            ErrorMessage = "Email is not valid")]
+      
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [RegularExpression("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$",
         ErrorMessage = "Password must be at least 8 characters and contain an uppercase, lowercase, digit, and special character")]
-      //  [DataType(DataType.Password, ErrorMessage = "Blah")]
+     
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirm Password is required")]
