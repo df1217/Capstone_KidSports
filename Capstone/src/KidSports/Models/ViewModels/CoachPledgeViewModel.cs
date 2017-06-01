32b796a77d119bb0127514c2fd6c8fd8d70802f9
df1217 <dffrank1217@gmail.com>
2017-06-01 08:16:42 -0700

    Validation Stuff

--- Capstone/src/KidSports/Models/ViewModels/CoachPledgeViewModel.cs
+++ Capstone/src/KidSports/Models/ViewModels/CoachPledgeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace KidSports.Models.ViewModels
     public class CoachPledgeViewModel
     {
         public string Direction { get; set; }
+        [RegularExpression("true", ErrorMessage = "Must click on the Coach Pledge link")]
+        public string HasClicked { get; set; }
         public int ApplicationID { get; set; }
 
         public bool IsInAgreement { get; set; }
