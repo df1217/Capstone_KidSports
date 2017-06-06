723913f3c65ebdb325667da1f9f5fef294e7c474
Emma3165 <anucharles@hotmail.com>
2017-06-06 12:48:52 -0700

    AdminTests

---
+++ Capstone/KidSports.Tests/AdminTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using KidSports.Models.ViewModels;
+using KidSports.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kidsports.Tests
+{
+    public class AdminTests
+    {
+      
+        private AdminController controller;
+
+
+        [Fact]
+        public void CanAddUser()
+        {
+            //Arrange
+            var vm = new UpdateUserViewModel();
+            vm.FirstName = "Paul";
+            vm.MiddleName = "S";
+            vm.LastName = "Jones";
+            vm.Email = "jonesp@gmail.com";
+            //vm.Role = "Sport manager";
+
+            //Act
+            controller.AddUser(vm);
+
+            //Assert
+            Assert.Equal("Paul", vm.FirstName);
+            Assert.Equal("Jones", vm.LastName);
+
+        }
+
+        [Fact]
+        public void CanDeleteUser()
+        { }
+
+
+    }
+}
---
+++ Capstone/src/KidSports/Models/ViewModels/UpdateUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using KidSports.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace KidSports.Models.ViewModels
+{
+    public class UpdateUserViewModel
+    {
+        
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+        public string Area { get; set; }
+
+       
+        
+
+       
+
+    }
+}
